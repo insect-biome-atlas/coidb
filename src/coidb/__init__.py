b3bdfff34329a2bb30505972ee202cf74f05d293
John Sundh <john.sundh@scilifelab.se>
2025-06-21 07:08:14 +0200

    Add function to init

--- src/coidb/__init__.py
+++ src/coidb/__init__.py
@@ -1,3 +1,17 @@
 # SPDX-FileCopyrightText: 2025-present John Sundh <john.sundh@scilifelab.se>
 #
 # SPDX-License-Identifier: MIT
+
+import gzip as gz
+
+
+def series_to_fasta(series, outfile, compress=False):
+    """
+    Writes a polars Series to a file in fasta format
+    """
+    if compress:
+        with gz.open(outfile, "wt") as fhout:
+            fhout.write("\n".join(series.to_list()))
+    else:
+        with open(outfile, "w") as fhout:
+            fhout.write("\n".join(series.to_list()))
