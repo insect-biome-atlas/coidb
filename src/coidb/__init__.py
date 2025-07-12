2944969420f5895976f4dfa6ab35457a0de8439a
John Sundh <john.sundh@scilifelab.se>
2025-07-12 08:54:29 +0200

    Add functions to init file

--- src/coidb/__init__.py
+++ src/coidb/__init__.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 import gzip as gz
+from tempfile import NamedTemporaryFile
 
 
 def series_to_fasta(series, outfile, compress=False):
@@ -15,3 +16,31 @@ def series_to_fasta(series, outfile, compress=False):
     else:
         with open(outfile, "w") as fhout:
             fhout.write("\n".join(series.to_list()))
+
+
+def get_header(f):
+    """
+    Returns header from input file
+    """
+    with open(f, "r") as fhin:
+        return fhin.readline().rstrip().split("\t")
+
+
+def extract_columns(f, indices):
+    """
+    Loops over each line in input file and only writes certain columns to a
+    temporary file.
+    """
+    fout = NamedTemporaryFile(mode="w", delete=False)
+    with open(f, "r") as fhin:
+        for line in fhin:
+            line = line.rstrip()
+            items = line.split("\t")
+            kept_items = []
+            for i in indices:
+                try:
+                    kept_items.append(items[i])
+                except IndexError:
+                    kept_items.append("")
+            fout.write("\t".join(kept_items) + "\n")
+    return fout.name
