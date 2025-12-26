7b34dddba2033936cd925b610eb76180903c9230
johnne <john.sundh@scilifelab.se>
2025-12-26 08:36:30 +0100

    Add multi option to bold2fasta script

--- src/coidb/scripts/bold2fasta.py
+++ src/coidb/scripts/bold2fasta.py
@@ -20,6 +20,9 @@ def main():
         "--single", action="store_true", help="Only output single-record BOLD bins"
     )
     parser.add_argument(
+        "--multi", action="store_true", help="Only output multi-record BOLD bins"
+    )
+    parser.add_argument(
         "--id_col",
         type=str,
         default="processid",
@@ -48,6 +51,18 @@ def main():
             .to_list()
         )
         df = df.filter(pl.col("bin_uri").is_in(single_record_bins))
+    elif args.multi:
+        multi_record_bins = (
+            df.filter(pl.col("bin_uri").str.starts_with("BOLD:"))
+            .group_by("bin_uri")
+            .len()
+            .filter(pl.col("len") > 1)
+            .select("bin_uri")
+            .collect(engine="streaming")
+            .to_series()
+            .to_list()
+        )
+        df = df.filter(pl.col("bin_uri").is_in(multi_record_bins))
     df.with_columns(s=">" + pl.col(args.id_col) + "\n" + pl.col(args.seq_col)).select(
         "s"
     ).sink_csv(args.outfile, include_header=False, quote_style="never")
