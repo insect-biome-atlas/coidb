9fa3401286ed208a7bc1bfd35b557538ec7cd6d7
John Sundh <john.sundh@scilifelab.se>
2026-02-05 14:44:52 +0100

    Refactoring to make fasta formatting more efficient

--- src/coidb/scripts/format.py
+++ src/coidb/scripts/format.py
@@ -3,83 +3,89 @@
 from argparse import ArgumentParser
 import polars as pl
 import gzip as gz
-from coidb import series_to_fasta, read_records
+import sys
 
 
-def format_dada2(df, outfile_toGenus, outfile_toSpecies, outfile_assignSpecies):
+def read_records(f):
     """
-    Generate files compatible with DADA2
+    Read records from fasta file
     """
-    df.with_columns(
-        s=">"
-        + pl.col("kingdom")
-        + ";"
-        + pl.col("phylum")
-        + ";"
-        + pl.col("class")
-        + ";"
-        + pl.col("order")
-        + ";"
-        + pl.col("family")
-        + ";"
-        + pl.col("genus")
-        + ";\n"
-        + pl.col("seq")
-    ).select("s").sink_csv(outfile_toGenus, include_header=False, quote_style="never")
-    # Generate a fasta up to the species level
-    df.with_columns(
-        s=">"
-        + pl.col("kingdom")
-        + ";"
-        + pl.col("phylum")
-        + ";"
-        + pl.col("class")
-        + ";"
-        + pl.col("order")
-        + ";"
-        + pl.col("family")
-        + ";"
-        + pl.col("genus")
-        + ";"
-        + pl.col("species")
-        + ";\n"
-        + pl.col("seq")
-    ).select("s").sink_csv(outfile_toSpecies, include_header=False, quote_style="never")
-    # Generate a fasta with only species
-    df.with_columns(
-        s=">" + pl.col("processid") + " " + pl.col("species") + "\n" + pl.col("seq")
-    ).select("s").sink_csv(
-        outfile_assignSpecies, include_header=False, quote_style="never"
+    records = []
+    bin_uris = []
+    if f.endswith(".gz"):
+        open_fn = gz.open
+        mode = "rt"
+    else:
+        open_fn = open
+        mode = "r"
+    with open_fn(f, mode) as fhin:
+        for line in fhin:
+            line = line.rstrip()
+            if line.startswith(">"):
+                record, bin_uri = line.lstrip(">").split(" ")
+                records.append(record)
+                bin_uris.append(bin_uri.lstrip("bin_uri:"))
+    return records, bin_uris
+
+
+def generate_kv_file(df, outfile, format="sintax"):
+    if format == "sintax":
+        df = df.with_columns(
+            value=";tax=k:"
+            + pl.col("kingdom")
+            + ",p:"
+            + pl.col("phylum")
+            + ",c:"
+            + pl.col("class")
+            + ",o:"
+            + pl.col("order")
+            + ",f:"
+            + pl.col("family")
+            + ",g:"
+            + pl.col("genus")
+            + ",s:"
+            + pl.col("species")
+            + ",t:"
+            + pl.col("bin_uri")
+        )
+    elif format == "dada2.toGenus":
+        df = df.with_columns(
+            value=pl.col("kingdom")
+            + ";"
+            + pl.col("phylum")
+            + ";"
+            + pl.col("class")
+            + ";"
+            + pl.col("order")
+            + ";"
+            + pl.col("family")
+            + ";"
+            + pl.col("genus")
+            + ";"
+        )
+    elif format == "dada2.toSpecies":
+        df = df.with_columns(
+            value=pl.col("kingdom")
+            + ";"
+            + pl.col("phylum")
+            + ";"
+            + pl.col("class")
+            + ";"
+            + pl.col("order")
+            + ";"
+            + pl.col("family")
+            + ";"
+            + pl.col("genus")
+            + ";"
+            + pl.col("species")
+            + ";"
+        )
+    elif format == "dada2.addSpecies":
+        df = df.with_columns(value=pl.col("species"))
+    df = df.with_columns(
+        key="bin_uri:" + pl.col("bin_uri"),
     )
-
-
-def format_sintax(df, outfile):
-    """
-    Output a fasta file compatible with SINTAX
-    Here the BOLD BIN is set as 'strain' ('t:' prefix)
-    """
-    df.with_columns(
-        s=">"
-        + pl.col("processid")
-        + ";tax=k:"
-        + pl.col("kingdom")
-        + ",p:"
-        + pl.col("phylum")
-        + ",c:"
-        + pl.col("class")
-        + ",o:"
-        + pl.col("order")
-        + ",f:"
-        + pl.col("family")
-        + ",g:"
-        + pl.col("genus")
-        + ",s:"
-        + pl.col("species")
-        + ",t:"
-        + pl.col("bin_uri")
-        + "\n"
-        + pl.col("seq")
-    ).select("s").sink_csv(outfile, include_header=False, quote_style="never")
+    df.select("key", "value").sink_csv(outfile, separator="\t", include_header=False)
 
 
 def format_qiime2(df, outfile):
@@ -125,77 +131,45 @@ def format_qiime2(df, outfile):
 def main():
     parser = ArgumentParser()
     parser.add_argument(
-        "--tsv",
+        "--fasta",
         type=str,
-        required=True,
-        help="TSV file with taxonomic info and sequences",
-    )
-    parser.add_argument(
-        "--fasta", type=str, required=False, help="Fasta file of sequences"
+        required=False,
+        help="Fasta file of sequences. Required when --format=qiime2",
     )
     parser.add_argument(
         "--consensus",
         type=str,
-        required=False,
+        required=True,
         help="TSV file with consensus taxonomy for BOLD BINs",
     )
-    subparsers = parser.add_subparsers(dest="format")
-    sintax_parser = subparsers.add_parser("sintax", help="SINTAX format options")
-    sintax_parser.add_argument(
-        "--outfile", type=str, required=True, help="SINTAX formatted fasta"
-    )
-    dada2_parser = subparsers.add_parser("dada2", help="DADA2 format options")
-    dada2_parser.add_argument(
-        "--outfile_toGenus",
+    parser.add_argument(
+        "--format",
         type=str,
-        required=True,
-        help="Output file for assignments up to genus",
+        default="sintax",
+        help="Output format. Choose from 'sintax', 'dada2 toGenus', 'dada2 toSpecies', 'dada2 addSpecies' or 'qiime2'",
+        choices=[
+            "sintax",
+            "dada2.toGenus",
+            "dada2.toSpecies",
+            "dada2.addSpecies",
+            "qiime2",
+        ],
     )
-    dada2_parser.add_argument(
-        "--outfile_toSpecies",
-        type=str,
-        required=True,
-        help="Output file for assignments up to species",
-    )
-    dada2_parser.add_argument(
-        "--outfile_assignSpecies",
-        type=str,
-        required=True,
-        help="Output file for assignSpecies",
-    )
-    qiime2_parser = subparsers.add_parser("qiime2", help="QIIME2 format options")
-    qiime2_parser.add_argument(
-        "--qiime2_outfile",
-        type=str,
-        required=True,
-        help="TSV file with taxonomic info for use with QIIME2",
-    )
+    parser.add_argument("-o", "--outfile", type=str, required=True, help="Output file")
     args = parser.parse_args()
-    if args.fasta:
-        records = read_records(args.fasta)
-    if args.consensus:
-        # Create a dataframe of only the records from the clustered fasta file
-        # and only keep the processid, bin_uri and seq columns
-        df = (
-            pl.scan_csv(args.tsv, separator="\t")
-            .filter(pl.col("processid").is_in(records))
-            .select(["processid", "bin_uri", "seq"])
-        )
-        # Read consensus taxonomy for BOLD BINs
-        consensus = pl.scan_csv(args.consensus, separator="\t")
-        # Merge dataframe with consensus, adding BOLD BIN taxonomy to records
-        df = consensus.join(df, on="bin_uri")
+    # Read consensus taxonomy for BOLD BINs
+    consensus = pl.scan_csv(args.consensus, separator="\t")
+    if args.format == "qiime2":
+        sys.stderr.write("Generating QIIME2 format file\n")
+        if not args.fasta:
+            sys.exit("Fasta file required when format=qiime2")
+        sys.stderr.write(f"Reading fasta headers from {args.fasta}\n")
+        records, bin_uris = read_records(args.fasta)
+        sys.stderr.write(f"{len(records)} headers stored\n")
+        df = pl.LazyFrame(data={"processid": records, "bin_uri": bin_uris})
+        df = df.join(consensus, on="bin_uri")
+        sys.stderr.write(f"Writing QIIME2 format file to {args.outfile}\n")
+        format_qiime2(df, args.outfile)
     else:
-        df = pl.scan_csv(args.tsv, separator="\t")
-    # Write the requested format
-    if args.format == "sintax":
-        format_sintax(df, args.outfile)
-    elif args.format == "dada2":
-        format_dada2(
-            df,
-            outfile_toGenus=args.outfile_toGenus,
-            outfile_toSpecies=args.outfile_toSpecies,
-            outfile_assignSpecies=args.outfile_assignSpecies,
-        )
-    elif args.format == "qiime2":
-        format_qiime2(df, args.qiime2_outfile)
+        sys.stderr.write(f"Writing {args.format} key-value file to {args.outfile}\n")
+        generate_kv_file(df=consensus, outfile=args.outfile, format=args.format)
